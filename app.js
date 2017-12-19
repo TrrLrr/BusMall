075e3d3ca2c777fba2ac14071a457b858175ccbc
Taylor Stemple <taylorstemple@gmail.com>
2017-12-19 13:55:08 -0800

    refactored into seperate functions

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ Product.allProducts = [];
 
 var lastShown = [];
 
+var names = [];
+
+var votesArr = [];
+
 //product variables
 var productOne = document.getElementById('product_one');
 
@@ -50,6 +54,92 @@ function ranNum() {
   return Math.floor(Math.random() * ((productArr.length-1) + 1));
 }
 
+
+//udpates lastshown arrays
+function updateArr() {
+  lastShown = [];
+
+  lastShown.push(randomProductOne);
+  lastShown.push(randomProductTwo);
+  lastShown.push(randomProductThree);
+}
+
+//test for duplicates
+
+function choiceTest() {
+  while(pass === false) {
+    randomProductOne = ranNum();
+    randomProductTwo = ranNum();
+    randomProductThree = ranNum();
+
+    while(randomProductTwo === randomProductOne) {
+      randomProductTwo = ranNum();
+    }
+
+    while(randomProductThree === randomProductTwo || randomProductThree === randomProductOne) {
+      randomProductThree = ranNum();
+    }
+
+    for(var i = 0; i < lastShown.length; i++) {
+      if(randomProductOne === lastShown[i]) {
+        pass = false;
+        console.log(pass);
+        break;
+      } else if (randomProductTwo === lastShown[i]) {
+        pass = false;
+        console.log(pass);
+        break;
+      } else if (randomProductThree === lastShown[i]) {
+        pass = false;
+        console.log(pass);
+        break;
+      } else {
+        pass = true;
+      }
+    }
+
+  }
+}
+
+//#############################################
+function renderProducts() {
+  productOne.innerHTML = '<input type="image" src="' + Product.allProducts[randomProductOne].filePath + '" name="first_img">';
+
+  productTwo.innerHTML = '<input type="image" src="' + Product.allProducts[randomProductTwo].filePath + '" name="second_img">';
+
+  productThree.innerHTML = '<input type="image" src="' + Product.allProducts[randomProductThree].filePath + '" name="third_img">';
+
+}
+//create the chart
+function renderChart() {
+
+  for(var i = 0; i < Product.allProducts.length; i++){
+    names.push(Product.allProducts[i].name);
+    votesArr.push(Product.allProducts[i].totalVotes);
+  }
+
+
+var canvas = document.getElementById('myChart');
+var ctx = canvas.getContext('2d');
+
+var chart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: names,
+    datasets: [{
+      label: 'Votes',
+      data: votesArr,
+      backgroundColor: ['rgba(#cf44aa, 0.5)','rgba(28, 231, 237, 0.5)','rgba(#e3ee0c, 0.5)','rgba(#ff0000, 0.51)','rgba(0, 34, 255, 0.53)','rgba(#9e00ff, 0.5)','rgba(#ff9100, 0.49)','rgba(#ff0091, 0.5)','rgba(#96ff00, 0.51)','rgba(222, 44, 44, 0.51)','rgba(41, 57, 166, 0.52)','rgba(53, 207, 110, 0.55)','rgba(114, 57, 236, 0.54)','rgba(86, 189, 171, 0.54)','rgba(255, 230, 4, 0.55)','rgba(192, 28, 241, 0.48)','rgba(69, 195, 10, 0.57)','rgba(39, 42, 150, 0.52)','rgba(153, 38, 223, 0.53)','rgba(96, 190, 3, 0.46)']
+    }]
+  },
+  options: {}
+
+
+})
+
+}
+
+
 //survey start
 function startSurvey() {
 
@@ -82,60 +172,27 @@ function mainSurveyOne() {
 
   event.preventDefault();
 
-
-
   Product.allProducts[randomProductOne].totalVotes++;
   voteCounter++;
   pass = false;
 
-  while(pass === false) {
-    randomProductOne = ranNum();
-    randomProductTwo = ranNum();
-    randomProductThree = ranNum();
+  choiceTest();
+  renderProducts();
+  updateArr();
 
-    while(randomProductTwo === randomProductOne) {
-      randomProductTwo = ranNum();
-    }
-
-    while(randomProductThree === randomProductTwo || randomProductThree === randomProductOne) {
-      randomProductThree = ranNum();
-    }
-
-    for(var i = 0; i < lastShown.length; i++) {
-      if(randomProductOne === lastShown[i]) {
-        pass = false;
-        console.log(pass);
-        break;
-      } else if (randomProductTwo === lastShown[i]) {
-        pass = false;
-        console.log(pass);
-        break;
-      } else if (randomProductThree === lastShown[i]) {
-        pass = false;
-        console.log(pass);
-        break;
-      } else {
-        pass = true;
-      }
-    }
-
+  if(voteCounter >= 25) {
+    renderChart()
+  } else {
+    console.log('keep truckin');
   }
-  productOne.innerHTML = '<input type="image" src="' + Product.allProducts[randomProductOne].filePath + '" name="first_img">';
-
-  productTwo.innerHTML = '<input type="image" src="' + Product.allProducts[randomProductTwo].filePath + '" name="second_img">';
-
-  productThree.innerHTML = '<input type="image" src="' + Product.allProducts[randomProductThree].filePath + '" name="third_img">';
-
-  lastShown = [];
-
-  lastShown.push(randomProductOne);
-  lastShown.push(randomProductTwo);
-  lastShown.push(randomProductThree);
-
-
-
 
 }
+
+
+
+
+
+
 
 function mainSurveyTwo() {
 
@@ -145,54 +202,19 @@ function mainSurveyTwo() {
   voteCounter++;
   pass = false;
 
+  choiceTest();
+  renderProducts();
+  updateArr();
 
-  while(pass === false) {
-    randomProductOne = ranNum();
-    randomProductTwo = ranNum();
-    randomProductThree = ranNum();
-
-    while(randomProductTwo === randomProductOne) {
-      randomProductTwo = ranNum();
-    }
-
-    while(randomProductThree === randomProductTwo || randomProductThree === randomProductOne) {
-      randomProductThree = ranNum();
-    }
-
-    for(var i = 0; i < lastShown.length; i++) {
-      if(randomProductOne === lastShown[i]) {
-        pass = false;
-        break;
-        console.log(pass);
-      } else if (randomProductTwo === lastShown[i]) {
-        pass = false;
-        break;
-        console.log(pass);
-      } else if (randomProductThree === lastShown[i]) {
-        pass = false;
-        console.log(pass);
-        break;
-      } else {
-        pass = true;
-      }
-    }
-
+  if(voteCounter >= 25) {
+    renderChart()
+  } else {
+    console.log('keep truckin');
   }
-  productOne.innerHTML = '<input type="image" src="' + Product.allProducts[randomProductOne].filePath + '" name="first_img">';
-
-  productTwo.innerHTML = '<input type="image" src="' + Product.allProducts[randomProductTwo].filePath + '" name="second_img">';
-
-  productThree.innerHTML = '<input type="image" src="' + Product.allProducts[randomProductThree].filePath + '" name="third_img">';
-
-  lastShown = [];
-
-  lastShown.push(randomProductOne);
-  lastShown.push(randomProductTwo);
-  lastShown.push(randomProductThree);
-
-
 
 }
+
+
 
 function mainSurveyThree() {
 
@@ -202,56 +224,19 @@ function mainSurveyThree() {
   voteCounter++;
   pass = false;
 
+  choiceTest();
+  renderProducts();
+  updateArr();
 
-  while(pass === false) {
-    randomProductOne = ranNum();
-    randomProductTwo = ranNum();
-    randomProductThree = ranNum();
-
-    while(randomProductTwo === randomProductOne) {
-      randomProductTwo = ranNum();
-    }
-
-    while(randomProductThree === randomProductTwo || randomProductThree === randomProductOne) {
-      randomProductThree = ranNum();
-    }
-
-    for(var i = 0; i < lastShown.length; i++) {
-      if(randomProductOne === lastShown[i]) {
-        pass = false;
-        console.log(pass);
-        break;
-      } else if (randomProductTwo === lastShown[i]) {
-        pass = false;
-        console.log(pass);
-        break;
-      } else if (randomProductThree === lastShown[i]) {
-        pass = false;
-        console.log(pass);
-        break;
-
-      } else {
-        pass = true;
-        console.log(pass);
-      }
-    }
-
+  if(voteCounter >= 25) {
+    renderChart()
+  } else {
+    console.log('keep truckin');
   }
-  productOne.innerHTML = '<input type="image" src="' + Product.allProducts[randomProductOne].filePath + '" name="first_img">';
-
-  productTwo.innerHTML = '<input type="image" src="' + Product.allProducts[randomProductTwo].filePath + '" name="second_img">';
-
-  productThree.innerHTML = '<input type="image" src="' + Product.allProducts[randomProductThree].filePath + '" name="third_img">';
-
-  lastShown = [];
-
-  lastShown.push(randomProductOne);
-  lastShown.push(randomProductTwo);
-  lastShown.push(randomProductThree);
-
-
 
 }
+
+
 
 
 
